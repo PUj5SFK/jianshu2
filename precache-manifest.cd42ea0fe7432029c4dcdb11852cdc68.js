906a0ddadcd3394471a8c6c7218687094ab31677
PUj5SFK <44357138+PUj5SFK@users.noreply.github.com>
2020-03-11 13:56:08 +0800

    project

---
+++ precache-manifest.cd42ea0fe7432029c4dcdb11852cdc68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637d8924c4ca309f15babc72aa0ca872",
+    "url": "/jianshu2/index.html"
+  },
+  {
+    "revision": "cedb7f79897c4aeb9fcf",
+    "url": "/jianshu2/static/js/2.924de3cb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/jianshu2/static/js/2.924de3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c449ad09b0dedcbea91",
+    "url": "/jianshu2/static/js/main.b22e9158.chunk.js"
+  },
+  {
+    "revision": "327529a5310b9554ef84",
+    "url": "/jianshu2/static/js/runtime-main.349e84a1.js"
+  },
+  {
+    "revision": "1acffbe9c3c70904125d4479dfbf5188",
+    "url": "/jianshu2/static/media/download.1acffbe9.png"
+  }
+]);
